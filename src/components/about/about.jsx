06571c9e21ceadf135ce88da619ed787062a633a
nahom <nahumtefera@gmail.com>
2018-04-07 18:21:09 -0700

    removed commented code

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -10,25 +10,18 @@ class About extends Component {
             <div className="about-container">
                 <div className="about-content-container">
                     <div className="about-main-image-container">
-                        <img className="about-main-image" src={require('../../images/about-img-2.png')} alt=""/>
+                        <img className="about-main-image" src={require('../../images/about-img.png')} alt=""/>
                     </div>
                     <div className="about-content">
                         <p className="about-content-text">
-
-                            • A La Turca Restaurant (US Restaurant Management) offers delicious dining and takeout to San Francisco, CA.
-                            <br/><br/>
-                            • A La Turca Restaurant (US Restaurant Management) is a cornerstone in the San Francisco community and has been recognized for its outstanding Mediterranean cuisine, excellent service and friendly staff.
-                            <br/><br/>
-                            • Our Mediterranean restaurant is known for its modern interpretation of classic dishes and its insistence on only using high quality fresh ingredients.
+                             A La Turca Restaurant (US Restaurant Management) offers delicious dining and takeout to San Francisco, CA. 
+                             <br/><br/>
+                             A La Turca Restaurant (US Restaurant Management) is a cornerstone in the San Francisco community and has been recognized for its outstanding Mediterranean cuisine, excellent service and friendly staff. 
+                             <br/><br/>
+                             Our Mediterranean restaurant is known for its modern interpretation of classic dishes and its insistence on only using high quality fresh ingredients.
 
                         </p>
                     </div>
-                    {/* <div className="about-image-container floating">
-                        <img className="about-image-item" src={require('../../images/face-picture.jpg')} alt="face owner"/>
-                    </div> */}
-                    {/* <p className="about-text">
-                    Whitmans Restaurant serves burgers and seasonal fare with a focus on fresh, local ingredients. Opened by native New Yorkers, the concept was born by friends whose vision was to provide the neighborhood with delicious comfort food in a cozy, intimate setting. The main attraction, modeled after the Minneapolis-originated phenomenon, the “Juicy Lucy,” features two thin beef short rib blend patties pinched together encasing a scoop of pimento cheese, the result: a molten core of cheese oozing from the patty. Whitmans introduced their take on the cult burger in 2010 to rave reviews. In addition, Whitmans offers a vast array of specialty crafted burgers including The Hound, Spicy Patty, and The Mangold just to name a few. While we are known for our burgers, some of our other most popular items include our East Villi Cheesesteak, Veggie Burger, and Kale Salad.
-                    </p> */}
                 </div>
             </div>
         )
